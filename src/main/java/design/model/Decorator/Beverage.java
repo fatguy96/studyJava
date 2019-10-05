c9e77321b75c9223a06bc9bee20a3876533c515e
shaochengli <chengli_s@126.com>
2019-10-05 23:06:27 +0800

    设计模型 打卡装饰者模式

---
+++ src/main/java/design/model/Decorator/Beverage.java
@@ -0,0 +1,11 @@
+package design.model.Decorator;
+
+public abstract class Beverage {
+    String description = "Unknown Beverage";
+
+    public String getDescription(){
+        return description;
+    }
+
+    public abstract double cost();
+}
---
+++ src/main/java/design/model/Decorator/CondimentDecorator.java
@@ -0,0 +1,5 @@
+package design.model.Decorator;
+
+public abstract class CondimentDecorator extends Beverage{
+    public abstract String getDescription();
+}
